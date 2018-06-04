edb42a58ef05f6e26c1e64f093e496588953176e
gauravarora1729 <varaugarora@gmail.com>
2018-06-04 21:08:09 +0530

    third commit

---
+++ sudokutest/sudokutest/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SolveButton = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GenerateButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SolveButton
+        '
+        Me.SolveButton.Location = New System.Drawing.Point(269, 392)
+        Me.SolveButton.Name = "SolveButton"
+        Me.SolveButton.Size = New System.Drawing.Size(75, 23)
+        Me.SolveButton.TabIndex = 1
+        Me.SolveButton.Text = "Solve"
+        Me.SolveButton.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(188, 392)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GenerateButton
+        '
+        Me.GenerateButton.Location = New System.Drawing.Point(12, 392)
+        Me.GenerateButton.Name = "GenerateButton"
+        Me.GenerateButton.Size = New System.Drawing.Size(135, 23)
+        Me.GenerateButton.TabIndex = 3
+        Me.GenerateButton.Text = "Generate Puzzle"
+        Me.GenerateButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(356, 427)
+        Me.Controls.Add(Me.GenerateButton)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.SolveButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SolveButton As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents GenerateButton As System.Windows.Forms.Button
+
+End Class
